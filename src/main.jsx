c32cdc35a298c6b9702ee15fc3abbf808d98eb29
onurergn57 <onurergn57@outlook.com>
2026-03-23 11:53:13 +0300

    Refactor App component rendering in main.jsx
    Simplify rendering of App component in main.jsx.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // .jsx uzantısını sildik, Vite otomatik bulur
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App' // Dikkat: ./App olarak çağırıyoruz
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+4.  **`index.html`:** Yukarıdaki yeni `index.html` kodunu kullan.
